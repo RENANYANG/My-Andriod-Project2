de96b131949ccc24edcc9802061fdeae8444f799
andrew.song <comsi02@gmail.com>
2024-04-08 23:50:43 -0400

    - completed marker

--- app/src/main/java/com/map1/group/project2/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/map1/group/project2/ui/maps/MapsFragment.kt
@@ -1,20 +1,33 @@
 package com.map1.group.project2.ui.maps
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.map1.group.project2.R
 
 class MapsFragment : Fragment() {
+
+    private lateinit var mMap: GoogleMap
+
+    companion object {
+        const val DEFAULT_ZOOM: Float = 12f
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -26,9 +39,24 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap = googleMap
+        val london = LatLng(42.9849, -81.24176)
+
+        mMap.addMarker(MarkerOptions().position(london).title("Marker in London"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(london, DEFAULT_ZOOM))
+
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isZoomGesturesEnabled = true
+            isScrollGesturesEnabled = true
+            isMapToolbarEnabled = true
+        }
+
+        onMapClicked()
     }
 
     override fun onCreateView(
@@ -43,5 +71,19 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun onMapClicked() {
+        Log.d("y_song", "onMapClicked")
+
+        mMap.setOnMapClickListener {
+            Log.d("y_song", "onMapClickListener")
+
+            val location = LatLng(it.latitude, it.longitude)
+            mMap.addMarker(MarkerOptions().position(location)
+                .title("Point")
+                .snippet(location.toString())
+            )
+        }
     }
 }
