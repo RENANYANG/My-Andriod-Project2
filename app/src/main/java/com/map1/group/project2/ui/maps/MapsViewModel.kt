fa5aa37d6e487fec44c265940754b7f375faa0e4
andrew.song <comsi02@gmail.com>
2024-04-08 20:56:58 -0400

    - dashboard --> maps

--- app/src/main/java/com/map1/group/project2/ui/maps/MapsViewModel.kt
+++ app/src/main/java/com/map1/group/project2/ui/maps/MapsViewModel.kt
@@ -1,13 +1,13 @@
-package com.map1.group.project2.ui.dashboard
+package com.map1.group.project2.ui.maps
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class MapsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Maps Fragment"
     }
     val text: LiveData<String> = _text
 }
