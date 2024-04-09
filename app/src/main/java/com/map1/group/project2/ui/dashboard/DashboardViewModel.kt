6914792dbacf95f19143f7a94dad42b40dd7b42b
andrew.song <comsi02@gmail.com>
2024-04-08 23:31:05 -0400

    - rollback map -> dashboard

--- app/src/main/java/com/map1/group/project2/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/map1/group/project2/ui/dashboard/DashboardViewModel.kt
@@ -1,10 +1,10 @@
-package com.map1.group.project2.ui.maps
+package com.map1.group.project2.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MapsViewModel : ViewModel() {
+class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is Maps Fragment"
