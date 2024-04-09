eeb6428a5cea55a6c46abf97932759c762a7fcbe
andrew.song <comsi02@gmail.com>
2024-04-08 21:04:43 -0400

    - notifications --> locations

--- app/src/main/java/com/map1/group/project2/ui/locations/LocationsViewModel.kt
+++ app/src/main/java/com/map1/group/project2/ui/locations/LocationsViewModel.kt
@@ -1,13 +1,13 @@
-package com.map1.group.project2.ui.notifications
+package com.map1.group.project2.ui.locations
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class LocationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is locations Fragment"
     }
     val text: LiveData<String> = _text
 }
